1117b9170888a0a1c3e9dd4fc7fa28af2b1e2eb8
Sébastien Boyron <sboyron@redhat.com>
2020-11-05 14:18:17 +0100

    Move interfaces configuration to a more readeable way

--- access-point/run.sh
+++ access-point/run.sh
@@ -1,63 +1,67 @@
 #!/usr/bin/env bashio
-set -x
+
+DEBUG=false
 
 HOSTAPD_CONFIG="/etc/hostapd/hostapd.conf"
 INTERFACES_CONFIG="/etc/network/interfaces"
 DNSMASQ_CONFIG="/etc/dnsmasq.conf"
 
 
-# SIGTERM-handler this funciton will be executed when the container receives the SIGTERM signal (when stopping)
-term_handler(){
-	bashio::log.info "Stop..."
-	ifdown $(bashio::config 'interface')
-	ip link set $(bashio::config 'interface') down
-	ip addr flush dev $(bashio::config 'interface') 
-	jobs=$(jobs -p)
-	if [ "$jobs" != "" ]; then
-		bashio::log.info "Kill all running jobs ($(jobs -p))"
-		kill $jobs
-	fi
-	echo "$? $LINENO"
-	exit 0
-}
-
 if_names="$(ls -d1  /sys/class/ieee80211/*/device/net/* | cut -d'/' -f8 | tr '\n' ' ')"
 bashio::log.info "List all available wireless interfaces : $if_names"
 
-# Setup signal handlers
-trap 'term_handler' SIGTERM
-trap 'term_handler' ERR
+function flush_net () {
 
+	ip link set $(bashio::config 'interface') down
+	ip addr flush dev $(bashio::config 'interface') 
+	ip addr show  $(bashio::config 'interface')
+
+}
 
 
 bashio::log.info "Starting..."
 
+# Networking part
+bashio::log.info "Configure interfaces..."
+
+bashio::log.debug "Create ${INTERFACES_CONFIG%/*} dir"
+mkdir -p ${INTERFACES_CONFIG%/*}
+
+bashio::log.debug "Create ${INTERFACES_CONFIG}"
+touch $INTERFACES_CONFIG
+
+cat <<EOF > $INTERFACES_CONFIG
+auto $(bashio::config 'interface')
+iface $(bashio::config 'interface') inet static
+  address $(bashio::config 'address')
+  netmask $(bashio::config 'netmask')
+  broadcast $(bashio::config 'broadcast')
+EOF
+
+$DEBUG && cat $INTERFACES_CONFIG
+
+echo "Initial state :"
+ip addr show  $(bashio::config 'interface')
+
+bashio::log.info "Reset latest interface status"
+flush_net
+
+bashio::log.info "Starting interface ...";
+ifup $(bashio::config 'interface')
+ip addr show  $(bashio::config 'interface')
+
+
+# Hostapd part
 bashio::log.info "Configure hostapd ..."
 sed -i "s/__INTERFACE__/$(bashio::config 'interface')/" $HOSTAPD_CONFIG
 sed -i "s/__SSID__/$(bashio::config 'ssid')/" $HOSTAPD_CONFIG
 sed -i "s/__CHANNEL__/$(bashio::config 'channel')/" $HOSTAPD_CONFIG
 sed -i "s/__WPA_PASSPHRASE__/$(bashio::config 'wpa_passphrase')/" $HOSTAPD_CONFIG
 
-echo "###################"
-cat $HOSTAPD_CONFIG
-echo "###################"
+$DEBUG && cat $HOSTAPD_CONFIG
 
 
-bashio::log.info "Configure interfaces..."
-sed -i "s/__INTERFACE__/$(bashio::config 'interface')/" $INTERFACES_CONFIG
-sed -i "s/__ADDRESS__/$(bashio::config 'address')/" $INTERFACES_CONFIG
-sed -i "s/__NETMASK__/$(bashio::config 'netmask')/" $INTERFACES_CONFIG
-sed -i "s/__BROADCAST__/$(bashio::config 'broadcast')/" $INTERFACES_CONFIG
 
-echo "###################"
-cat $INTERFACES_CONFIG
-echo "###################"
-
-if ifdown $(bashio::config 'interface'); then 
-	echo "Interface stopped !"
-fi
-bashio::log.info "Starting interface ...";
-ifup $(bashio::config 'interface')
 
 
 bashio::log.info "Configuring dnsmasq..."
@@ -94,30 +98,49 @@ echo "# Route" >> ${DNSMASQ_CONFIG}
 echo "dhcp-option=3,$(bashio::config 'address')" >> ${DNSMASQ_CONFIG}
 
 
-echo "###################"
-cat $DNSMASQ_CONFIG
-echo "###################"
+$DEBUG && cat $DNSMASQ_CONFIG
 
 
+# Executions
 
-(\
- bashio::log.info "Starting HostAP daemon ..."; \
- hostapd  /etc/hostapd/hostapd.conf | sed 's/^/hostapd: /'; \
- echo "hostapd crashed !";\
- kill $$ \
-)&
+bashio::log.info "Starting HostAP daemon ..." 
+hostapd  -B -P /tmp/hostapd.pid /etc/hostapd/hostapd.conf 
 
 
-ip addr show $(bashio::config 'interface')
+$DEBUG && ip addr show $(bashio::config 'interface')
 
-(\
- bashio::log.info "Starting dnsmasq...";\
- exec dnsmasq -C "${DNSMASQ_CONFIG}" -z < /dev/null | sed 's/^/dnsmasq: /';\
- echo "dnsmasq crashed !";\
- kill $$\
-)&
+bashio::log.info "Starting dnsmasq..."
+dnsmasq -C "${DNSMASQ_CONFIG}" -z -x /tmp/dnsmasq.pid
+
+while :; do
+	if ! ps aux | grep -q $(cat /tmp/hostapd.pid); then
+		bashio::log.error "hostapd is down !"
+		flush_net 
+		kill $(cat /tmp/hostapd.pid)
+		exit 1
+	fi
+
+	if ! ps aux | grep -q $(cat /tmp/hostapd.pid); then
+		bashio::log.error "dnsmasq is down !"
+		flush_net
+		kill $(cat /tmp/hostapd.pid)
+		exit 2
+	fi
 
 
-wait
+	sleep 10
+done
+
+# SIGTERM-handler this funciton will be executed when the container receives the SIGTERM signal (when stopping)
+term_handler(){
+	bashio::log.info "Stop..."
+	jobs=$(jobs -p)
+	if [ "$jobs" != "" ]; then
+		bashio::log.info "Kill all running jobs ($(jobs -p))"
+		kill $jobs
+	fi
+	echo "$? $LINENO"
+	exit 0
+}
 
 
